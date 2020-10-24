351a5cb71e4344dd9b5ea588236db8101a59cd52
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-24 13:27:59 +0900

    some,everyメソッド、filterメソッド

--- sample.js
+++ sample.js
@@ -1,14 +1,20 @@
 var data = [2, 3, 4, 5];
 
-// 配列の内容を順番に処理
-// forEach文(value=値,index=番号,array=元の配列)
-data.forEach(function (value, index, array) {
-    console.log(value * value);
-}); //4,9,16,25
-
-// 配列を指定のルールで加工
-// mapメソッド
-var result1 = data.map(function (value, index, array) {
-    return value * value;
+// 要素がひとつでもあってるか
+// someメソッド(evaryメソッド:全ての要素が一致しているか)
+var result1 = data.some(function (value, index, array) {
+    return value % 3 === 0;
 });
-console.log(result1); //[4,9,16,25]
+
+if (result1) {
+    console.log('3の倍数が見つかりました');
+} else {
+    console.log('3の倍数は見つかりませんでした');
+} //3の倍数が見つかりました
+
+// 条件にあった要素のみ取り出す
+// filterメソッド
+var result2 = data.filter(function (value, index, array) {
+    return value % 2 == 1;
+});
+console.log(result2); //[3,5]
