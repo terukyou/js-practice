38b23a993d66c6886bf67d187fd22ec6ae883506
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-26 15:33:38 +0900

    6.4.4 ラジオボタンの初期値

--- sample.js
+++ sample.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded',
     function () {
+        // ラジオボタンの初期値の設定
+        var setRadioValue = function (name, value) {
+            var elems = document.getElementsByName(name);
+            for (var i = 0, len = elems.length; i < len; i++) {
+                var elem = elems.item(i);
+                if (elem.value === value) { //値が初期値を同じとき
+                    elem.checked = true;
+                    break;
+                }
+            }
+        };
+        setRadioValue('food', '餃子');
         var getRadioValue = function (name) {
             var result = '';
             var elems = document.getElementsByName(name);
