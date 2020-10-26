329ddee89ef4c66a76acda766d113e4ae97e46cf
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-26 14:27:15 +0900

    6.4.2 入力・選択ボックスの値取得

--- sample.js
+++ sample.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // HTML文字列として認識しない
-    document.getElementById('result_text').textContent = '<a href="https://wings.msn.to/">WINGSプロジェクト</a>';
-    // HTML文字列として認識
-    document.getElementById('result_html').innerHTML = '<a href="https://wings.msn.to/">WINGSプロジェクト</a>';
-}, false);
+document.addEventListener('DOMContentLoaded',
+    function () {
+        document.getElementById('btn').addEventListener('click', //btnをクリックしたとき
+            function () {
+                var name = document.getElementById('name');
+                console.log(name.value);
+            }, false);
+    }, false);
