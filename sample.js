42af97c74f2b899997e65b673c74020fedcdba11
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-26 12:34:46 +0900

    6.2.3 イベントハンドラーの登録

--- sample.js
+++ sample.js
@@ -1,18 +1,7 @@
-// select#foodを取得
-var s = document.getElementById('food');
-
-// firstChild/nextSibling
-var child = s.firstChild;
-while (child) { //子ノードがある限りループ
-    if (child.nodeType === 1) {
-        console.log(child.value);
+// ページロード時に実行されるイベントハンドラー(イベントに対応してその処理内容を定義するコードの塊)を登録
+window.onload = function () {
+    // btnをクリックした時に実行されるイベントハンドラーを登録
+    document.getElementById('btn').onclick = function () {
+        window.alert('ボタンがクリックされました');
     }
-    child = child.nextSibling; //次の子ノードの取得
-}
-
-// firstElementChild/nextElementSibling
-var child2 = s.firstElementChild; //要素のみ取得できるのでif(chiled.nodeType===1は必要なし)
-while (child2) {
-    console.log(child2.value);
-    child2 = child2.nextElementSibling;
 }
