a3c0c897b06908c888b25ba0fe010d24b1cd6af5
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-26 15:06:07 +0900

    6.4.2 チェックボックスの値を取得する

--- sample.js
+++ sample.js
@@ -2,7 +2,14 @@ document.addEventListener('DOMContentLoaded',
     function () {
         document.getElementById('btn').addEventListener('click', //btnをクリックしたとき
             function () {
-                var name = document.getElementById('name');
-                console.log(name.value);
+                var result = []; //選択値を格納するための配列
+                var foods = document.getElementsByName('food');
+                for (var i = 0, len = foods.length; i < len; i++) {
+                    var food = foods.item(i);
+                    if (food.checked) { //チェックの状態か確認
+                        result.push(food.value);
+                    }
+                }
+                window.alert(result.toString());
             }, false);
     }, false);
