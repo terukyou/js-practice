43fdfec02682436f9fa767bd02c19b815b653e26
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-26 11:57:18 +0900

    querySelector

--- sample.js
+++ sample.js
@@ -1,11 +1,5 @@
-// idをキーに要素を取得
-var current = new Date();
-var result = document.getElementById('result');
-result.textContent = current.toLocaleString();
-
-// ページの全てのアンカータグを取得
-var list = document.getElementsByTagName('a');
-// hrefをログに出力
+// idがlist配下のexternalクラスのみ抽出
+var list = document.querySelectorAll('#list .external');
 for (var i = 0, len = list.length; i < len; i++) {
-    console.log(list.item(i).href);
+    console.log(list[i].href);
 }
