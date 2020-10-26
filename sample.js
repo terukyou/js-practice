e8e4ce9753656b0f8ea73a4d7f99f039ec50ca39
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 07:34:15 +0900

    テキストファイルの内容を取得

--- sample.js
+++ sample.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded',
     function () {
         document.getElementById("file").addEventListener('change',
-            function () {
-                var inputs = document.getElementById("file").files;
-                for (var i = 0, len = inputs.length; i < len; i++) {
-                    var input = inputs[i];
-                    console.log('ファイル名:' + input.name);
-                    console.log('種類:' + input.type);
-                    console.log('サイズ:' + input.size);
-                    console.log('最終更新日:' + input.lastModifiedDate);
-                }
+            function (e) {
+                // 単一選択なので、0で決め打ち
+                var input = document.getElementById("file").files[0];
+                var reader = new FileReader();
+                // テキストファイルの内容を取得
+                reader.addEventListener('load', //ファイルの読み込みが実行されてるわけじゃない
+                    function () {
+                        document.getElementById("result").textContent = reader.result;
+                    }, true);
+                reader.addEventListener('error', //エラーが発生したとき
+                    function () {
+                        console.log(reader.error.message);
+                    }, true);
+                reader.readAsText(input, 'UTF-8'); //ファイルの読み込み
+                reader.abort();
             })
     }, true);
