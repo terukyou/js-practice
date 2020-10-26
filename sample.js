6014bf90b1b7fec5d6ee3c250360671f1dddecef
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-26 12:12:16 +0900

    6.2 ノードウォーキング(childNodes)

--- sample.js
+++ sample.js
@@ -1,5 +1,10 @@
-// idがlist配下のexternalクラスのみ抽出
-var list = document.querySelectorAll('#list .external');
-for (var i = 0, len = list.length; i < len; i++) {
-    console.log(list[i].href);
+// select#foodを取得
+var s = document.getElementById('food');
+// 要素直下のノード群取得
+var opts = s.childNodes;
+for (var i = 0, len = opts.length; i < len; i++) {
+    var opt = opts.item(i);
+    if (opt.nodeType === 1) { //要素以外も取得できるので要素のみ抽出
+        console.log(opt.value);
+    }
 }
