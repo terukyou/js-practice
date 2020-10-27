a89a4cd79045288a8b0184a287eb7a2fb7915c23
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 16:25:10 +0900

    スタックトレース

--- sample.js
+++ sample.js
@@ -1,20 +1,13 @@
-console.log('初めまして、私は %s です。 %d 歳です。', '山田太郎', 30);
-
-// groupからgroupEndまでひとまとめで表示してくれる
-console.group('上位グループ');
-for (var i = 0; i < 3; i++) {
-    console.group('下位グループ' + i);
-    for (var j = 0; j < 3; j++) {
-        console.log(i, j);
-    }
-    console.groupEnd();
+// スタックトレースが分かる
+function call1() {
+    call2();
 }
-console.groupEnd();
-
-// 何回実行されたかカウント
-for (var i = 0; i < 3; i++) {
-    for (var i = 0; i < 3; i++) {
-        console.count('LOOP');
-    }
-    console.count('LOOP');
+function call2() {
+    call3();
 }
+function call3() {
+    console.trace();
+}
+
+call1();
+
