7913abeeb5ee1dcca5d3aaaca194f2c2b486f22c
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 17:25:01 +0900

    7.4.1 AjaxのHello,World

--- sample.js
+++ sample.js
@@ -1,4 +1,20 @@
-var storage = localStorage;
-storage.setItem('fruit1', 'リンゴ');
-storage.fruit2 = 'みかん';
-storage['fruit3'] = 'ブドウ'; //おすすめ！
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn').addEventListener('click', function () {
+        var result = document.getElementById('result');
+        var xhr = new XMLHttpRequest();
+        console.log(document.getElementById('name').value);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    result.textContent = xhr.responseText;
+                } else {
+                    result.textContent = 'サーバーエラーが発生しました';
+                }
+            } else {
+                result.textContent = '通信中...';
+            }
+        };
+        xhr.open('GET', 'sample.php?name=' + encodeURIComponent(document.getElementById('name').value), true);
+        xhr.send(null);
+    }, false);
+}, false);
