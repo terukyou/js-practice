1147b480f66d80eff73ab79250bd27edaa93e4cd
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 14:52:16 +0900

    handleEventメソッド

--- sample.js
+++ sample.js
@@ -2,13 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var data = {
         title: 'Javaポケットリファレンス',
         price: 2680,
-        show: function () {
-            //ここのthisはオブジェクト自身じゃなくボタンを指している
+        handleEvent: function () {
             console.log(this.title + '/' + this.price + '円');
         }
     };
-    document.getElementById('btn').addEventListener('click', data.show, false); //「/undefined円」
-    document.getElementById('btn').addEventListener('click', data.show.bind(data), false); //「Javaポケットリファレンス/2680円」
+    document.getElementById('btn').addEventListener('click', data, false);
 }, false);
 
 
