203a81a10d12661f6b8fe094d9289cb17c281291
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 15:41:55 +0900

    setTimeoutメソッド

--- sample.js
+++ sample.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // タイマーを設置
-    var timer = window.setInterval(function () {
-        var dat = new Date();
-        // 現在の時刻をresultに表示する
-        document.getElementById('result').textContent = dat.toLocaleTimeString();
-    }, 1000);
-
-    // タイマー処理を中止
-    document.getElementById('btn').addEventListener('click', function () {
-        window.clearInterval(timer);
-    }, false);
-}, false);
+function hoge() {
+    console.log('あいうえお');
+    setTimeout(function () {
+        console.log('かきくけこ');
+    }, 3000); //3秒後に「かきくけこ」
+    console.log('さしすせそ');
+}
+hoge();
