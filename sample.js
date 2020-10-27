0919a769c7f2ed16b71b269ad77b945e3166c00e
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 16:04:01 +0900

    7.1.7 chromeか判定

--- sample.js
+++ sample.js
@@ -1,12 +1,3 @@
-var count = 0;
-var result = document.getElementById('result');
-document.getElementById('btn').addEventListener('click', function () {
-    result.textContent = ++count;
-    // 履歴を追加
-    history.pushState(count, null, 'file:///C:/xampp/htdocs/git/js-practice/sample.html?' + count);
-});
-// 戻るボタンでページを前の状態に戻す
-window.addEventListener('popstate', function (e) {
-    count = e.state;
-    result.textContent = count;
-});
+var agent = window.navigator.userAgent.toLowerCase();
+var chrome = (agent.indexOf('chrome') > -1) && (agent.indexOf('edge') === -1) && (agent.indexOf('opr') === -1);
+console.log('Chrome:' + chrome);
