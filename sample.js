b1f745f8a46e9bc809d80cf96ec9241748616579
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 16:21:01 +0900

    7.2.2 ログメソッド(group/groupEnd,countメソッド)

--- sample.js
+++ sample.js
@@ -1,3 +1,20 @@
-var agent = window.navigator.userAgent.toLowerCase();
-var chrome = (agent.indexOf('chrome') > -1) && (agent.indexOf('edge') === -1) && (agent.indexOf('opr') === -1);
-console.log('Chrome:' + chrome);
+console.log('初めまして、私は %s です。 %d 歳です。', '山田太郎', 30);
+
+// groupからgroupEndまでひとまとめで表示してくれる
+console.group('上位グループ');
+for (var i = 0; i < 3; i++) {
+    console.group('下位グループ' + i);
+    for (var j = 0; j < 3; j++) {
+        console.log(i, j);
+    }
+    console.groupEnd();
+}
+console.groupEnd();
+
+// 何回実行されたかカウント
+for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 3; i++) {
+        console.count('LOOP');
+    }
+    console.count('LOOP');
+}
