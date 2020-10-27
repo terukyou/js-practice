8581e1cb150e5dd587304f0310d907e605fc9948
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 10:19:55 +0900

    イベントリスナーを削除

--- sample.js
+++ sample.js
@@ -1,10 +1,13 @@
-window.onload =
+document.addEventListener('DOMContentLoaded',
     function () {
         var btn = document.getElementById('btn');
-        btn.onclick =
-            function () {
-                window.alert('こんにちは世界');
-            };
-        btn.onclick = null;
+        var listener = function () {
+            window.alert('こんにちは、世界！');
+        };
+        // イベントリスナーを登録
+        btn.addEventListener('click', listener, false);
+        // イベントリスナーを削除
+        // btn.removeEventListener('click', listener, false);
+    }, false);
 
-    }
+
