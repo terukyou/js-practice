b8c10893e8a4d4190d7361cfd3190af24da35264
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 15:37:38 +0900

    7.1.3 タイマー機能

--- sample.js
+++ sample.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('fm').addEventListener('submit', function (e) {
-        if (!window.confirm('ページを送信してもいいですか？')) {
-            e.preventDefault();
-        }
-    }, false)
+    // タイマーを設置
+    var timer = window.setInterval(function () {
+        var dat = new Date();
+        // 現在の時刻をresultに表示する
+        document.getElementById('result').textContent = dat.toLocaleTimeString();
+    }, 1000);
+
+    // タイマー処理を中止
+    document.getElementById('btn').addEventListener('click', function () {
+        window.clearInterval(timer);
+    }, false);
 }, false);
