cbbe903d179143f6553fa1cba66b0334dab13f08
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 10:51:48 +0900

    イベント処理の伝播とキャンセル

--- sample.js
+++ sample.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded',
     function () {
-        document.getElementById('key').addEventListener('keydown',
+        document.getElementById('inner').addEventListener('click',
             function (e) {
-                console.log('キーコード:' + e.keyCode);
+                window.alert('#innerリスナーが発生');
+                // e.stopPropagation(); //#outerリスナーが発生のやつがキャンセル
+            }, false);
+        document.getElementById('inner').addEventListener('click',
+            function (e) {
+                window.alert('#inner2リスナーが発生');
+            }, false);
+        document.getElementById('outer').addEventListener('click',
+            function (e) {
+                window.alert('#outer リスナーが発生しました');
             }, false);
     }, false);
 
