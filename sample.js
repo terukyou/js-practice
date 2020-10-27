14133604e4cc963f75f89b6db49233b109070a91
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 10:01:42 +0900

    toggleメソッド

--- sample.js
+++ sample.js
@@ -3,17 +3,8 @@ document.addEventListener('DOMContentLoaded',
         var elem = document.getElementById('elem');
         elem.addEventListener('click', // マウスが乗った時
             function () {
-                // 空白区切りの文字列を分割
-                var classes = this.className.split(' ');
-                // highlightが存在する位置を検出
-                var index = classes.indexOf('highlight');
-                console.log(index);
-                if (index === -1) {
-                    classes.push('highlight');
-                } else {
-                    classes.splice(index, 1);
-                }
-                this.className = classes.join(' ');
+                // 条件演算子不要、文字列の分割不要
+                this.classList.toggle("highlight");
             }, false);
     }, false);
 
