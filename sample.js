8b1111ca0f5c88f283459ffcf76148490ff098ff
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 09:58:56 +0900

    複数のスタイルクラスの時

--- sample.js
+++ sample.js
@@ -3,7 +3,17 @@ document.addEventListener('DOMContentLoaded',
         var elem = document.getElementById('elem');
         elem.addEventListener('click', // マウスが乗った時
             function () {
-                this.className = (this.className === 'highlight' ? '' : 'highlight');
+                // 空白区切りの文字列を分割
+                var classes = this.className.split(' ');
+                // highlightが存在する位置を検出
+                var index = classes.indexOf('highlight');
+                console.log(index);
+                if (index === -1) {
+                    classes.push('highlight');
+                } else {
+                    classes.splice(index, 1);
+                }
+                this.className = classes.join(' ');
             }, false);
     }, false);
 
