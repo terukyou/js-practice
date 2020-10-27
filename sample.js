4d2871185d7eeeeb1ea1897d760ba54964f21f86
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 09:26:02 +0900

    HTMLCollection,Nodeistオブジェは生きたオブジェクト

--- sample.js
+++ sample.js
@@ -1,31 +1,14 @@
 document.addEventListener('DOMContentLoaded',
     function () {
-        var list = document.getElementById('list');
-        var pic = document.getElementById('pic');
-        var del = document.getElementById('del');
-
-        list.addEventListener('click',
-            function (e) {
-                var isbn = e.target.getAttribute('data-isbn');
-                if (isbn) {
-                    var img = document.createElement('img');
-                    img.src = 'http://www.wings.msn.to/books/' + isbn + '/' + isbn + '.jpg';
-                    img.alt = e.target.textContent;
-                    img.height = 150;
-                    img.width = 108;
-                    if (pic.getElementsByTagName('img').length > 0) {
-                        pic.replaceChild(img, pic.lastChild);
-                    } else {
-                        del.disabled = false;
-                        pic.appendChild(img);
-                    }
-                }
-            }, false);
-        del.addEventListener('click',
-            function () {
-                pic.removeChild(pic.lastChild);
-                del.disabled = true;
-            }, false);
+        var second = document.getElementById('second');
+        var li = document.getElementsByTagName('li');
+        for (var i = 0, len = li.length; i < len; i++) { //i<li.lengthだと無限ループに...
+            var item = li.item(i);
+            var new_li = document.createElement('li');
+            var new_text = document.createTextNode(item.textContent);
+            new_li.appendChild(new_text);
+            second.appendChild(new_li);
+        }
     }, false);
 
 
