2c2cb9a49220506395ca6a1eac1e17a98fcd13a6
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-27 17:48:48 +0900

    7.4.3~7.4.5 ?????????????

--- sample.js
+++ sample.js
@@ -1,12 +1,30 @@
+// なんも分からん
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('btn').addEventListener('click', function () {
         var result = document.getElementById('result');
         var xhr = new XMLHttpRequest();
-        console.log(document.getElementById('name').value);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
-                    result.textContent = xhr.responseText;
+                    var data = JSON.parse(xhr.responseText);
+                    if (data === null) {
+                        result.textContent = 'ブックマークは存在しませんでした';
+                    } else {
+                        var bms = data.bookmarks;
+                        var ul = document.createElement('ul');
+                        for (var i = 0; i < bms.length; i++) {
+                            var li = document.createElement('li');
+                            var anchor = document.createElement('a');
+                            anchor.href = 'http://b.hatena.ne.jp/' + bms[i].user
+                            var text = document.createTextNode(
+                                bms[i].user + ' ' + bms[i].comment
+                            );
+                            anchor.appendChild(text);
+                            li.appendChild(anchor);
+                            ul.appendChild(li);
+                        }
+                        result.replaceChild(ul, result.firstChild);
+                    }
                 } else {
                     result.textContent = 'サーバーエラーが発生しました';
                 }
