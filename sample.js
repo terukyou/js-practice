6ec050269211bb944469b9be70e87d5a352b18be
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-28 19:45:31 +0900

    静的プロパティ/メソッド

--- sample.js
+++ sample.js
@@ -1,13 +1,19 @@
-var Member = function (firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+var Area = function () { };
+// 静的プロパティ/メソッド インスタンス生成せんでもいい
+Area.version = '1.0';
+Area.triangle = function (base, height) {
+    return base * height / 2;
 };
-// オブジェクトリテラルでプロトタイプを定義
-Member.prototype = {
-    getName: function () {
-        return this.firstName + ' ' + this.lastName;
-    },
-    toString: function () {
-        return this.lastName + this.firstName;
-    }
+Area.diamond = function (width, height) {
+    return width * height / 2;
 };
+console.log(Area.version);
+console.log(Area.triangle(5, 3));
+
+// インスタンス化(この時静的メンバーは追加されない)
+var a = new Area();
+
+console.log(a.version); //undefined
+console.log(a.triangle(5, 3)); //エラー
+// 静的プロパティは、読み取り専用の用途に使え（は？？？？？？？
+// 静的メソッドの中ではthis使えん（は？？？？？？？？？？？
