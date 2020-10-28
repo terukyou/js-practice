eaf9c1da415aafa943a9bf28ed8d8c7360bfb375
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-28 17:24:51 +0900

    プロパティを後付けさせないsealメソッド

--- sample.js
+++ sample.js
@@ -1,6 +1,7 @@
 var Member = function (firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+    Object.seal(this); //後からプロパティを追加・削除したらエラーとなる
 };
 
 var mem = new Member('たけし', '山田');
