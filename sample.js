e93922d0b9a2e5a5346f60b7335333ae3017f76c
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-28 19:12:04 +0900

    5.2.5 オブジェクトリテラルでプロトタイプを定義

--- sample.js
+++ sample.js
@@ -1,14 +1,13 @@
-// プロトタイプのメンバー削除
-var Member = function () { };
-Member.prototype.sex = '男';
-var mem1 = new Member();
-var mem2 = new Member();
-console.log(mem1.sex + '|' + mem2.sex); //男❘男
-
-// delete Member.prototype.sex; //すべてのインスタンスのsexプロパティも削除
-
-mem2.sex = undefined; //「インスタンス単位」でプロトタイプのメンバーを削除
-// →値を強制的に未定義にしているだけなのでsexプロパティは存在するものとして表示される
-
-console.log(mem1.sex + '|' + mem2.sex); //男|undefined
-
+var Member = function (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+};
+// オブジェクトリテラルでプロトタイプを定義
+Member.prototype = {
+    getName: function () {
+        return this.firstName + ' ' + this.lastName;
+    },
+    toString: function () {
+        return this.lastName + this.firstName;
+    }
+};
