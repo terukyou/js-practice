be223ea36c1bdb796bafc14eda6205339a331470
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-28 19:02:02 +0900

    プロトタイプのメンバー削除

--- sample.js
+++ sample.js
@@ -1,15 +1,14 @@
-// インスタンスのメンバー追加や削除はプロトタイプに影響しない
-// プロパティの変更
+// プロトタイプのメンバー削除
 var Member = function () { };
 Member.prototype.sex = '男';
 var mem1 = new Member();
 var mem2 = new Member();
 console.log(mem1.sex + '|' + mem2.sex); //男❘男
-mem2.sex = '女';
-// プロトタイプの参照はインスタンスにあるか確認してから
-console.log(mem1.sex + '|' + mem2.sex); //男|女
 
-// プロパティの削除
-delete mem1.sex //インスタンスになくてもプロトタイプまでは削除しにいかない
-delete men2.sex
-console.log(mem1.sex + '|' + mem2.sex); //男|男
+// delete Member.prototype.sex; //すべてのインスタンスのsexプロパティも削除
+
+mem2.sex = undefined; //「インスタンス単位」でプロトタイプのメンバーを削除
+// →値を強制的に未定義にしているだけなのでsexプロパティは存在するものとして表示される
+
+console.log(mem1.sex + '|' + mem2.sex); //男|undefined
+
