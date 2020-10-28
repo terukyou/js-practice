05a67acb70126c55ddd5ff9781e55bca5c498b47
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-28 19:52:13 +0900

    5.3.1 プロトタイプチェーン

--- sample.js
+++ sample.js
@@ -1,19 +1,18 @@
-var Area = function () { };
-// 静的プロパティ/メソッド インスタンス生成せんでもいい
-Area.version = '1.0';
-Area.triangle = function (base, height) {
-    return base * height / 2;
+var Animal = function () { };
+Animal.prototype = {
+    walk: function () {
+        console.log('トコトコ');
+    }
 };
-Area.diamond = function (width, height) {
-    return width * height / 2;
+
+var Dog = function () {
+    Animal.call(this);
 };
-console.log(Area.version);
-console.log(Area.triangle(5, 3));
+Dog.prototype = new Animal();
+Dog.prototype.bark = function () {
+    console.log('わんわん');
+}
 
-// インスタンス化(この時静的メンバーは追加されない)
-var a = new Area();
-
-console.log(a.version); //undefined
-console.log(a.triangle(5, 3)); //エラー
-// 静的プロパティは、読み取り専用の用途に使え（は？？？？？？？
-// 静的メソッドの中ではthis使えん（は？？？？？？？？？？？
+var d = new Dog();
+d.walk(); //トコトコ
+d.bark(); //わんわん
