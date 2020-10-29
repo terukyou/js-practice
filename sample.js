b37d1fba173b9a43db35300af63d423f55f9e7dd
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 11:07:40 +0900

    5.5.2 オブジェクトリテラル(連想配列)の改善

--- sample.js
+++ sample.js
@@ -1,40 +1,8 @@
-class Member {
-    // コンストラクタの定義
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+let member = {
+    name: '山田太郎',
+    birth: new Date(1970, 5, 25),
+    toString() {
+        return this.name + ' 誕生日:' + this.birth.toLocaleDateString()
     }
-    // プロパティの定義
-    get firstName() {
-        return this._firstName;
-    }
-    set firstName(value) {
-        this._firstName = value;
-    }
-    get lastName() {
-        return this._lastName;
-    }
-    set lastName(value) {
-        this._lastName = value;
-    }
-    // メソッドの定義
-    getName() {
-        return this.lastName + this.firstName;
-    }
-}
-// クラスの継承
-class BusinessMember extends Member{
-    constructor(firstName, lastName, clazz) {
-        super(firstName, lastName); //基底クラスにプロパティの設定をゆだねる
-        this.clazz = clazz;
-    }
-    getName() { //MemberのgetNameを上書き
-        return super.getName() + '　役職:' + this.clazz;
-    }
-    work() {
-        return this.getName() + 'は働いています';
-    }
-}
-let bm = new BusinessMember('太郎', '山田','課長');
-console.log(bm.getName());
-console.log(bm.work());
+};
+console.log(member.toString());
