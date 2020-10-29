1d20ab57b0691912477a603edaaa0e13e22ce65d
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 17:58:11 +0900

    6.5.2 新規にノードを作成する

--- sample.js
+++ sample.js
@@ -1,11 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('file').addEventListener('change', function (e) {
-        var input = document.getElementById('file').files[0];
-        var reader = new FileReader();
-        reader.addEventListener('load', function () {
-            document.getElementById('result').src = reader.result;
-            console.log(reader.result);
-        }, true);
-        reader.readAsDataURL(input);
+document.addEventListener('DOMContentLoaded',
+    function () {
+        document.getElementById('btn').addEventListener('click', function () {
+            var name = document.getElementById('name');
+            var url = document.getElementById('url');
+
+            var anchor = document.createElement('a');
+            anchor.href = url.value;
+            var text = document.createTextNode(name.value);
+            anchor.appendChild(text);
+            var br = document.createElement('br');
+            var list = document.getElementById('list');
+            list.appendChild(anchor);
+            list.appendChild(br);
+        }, false);
     }, true);
-}, false);
