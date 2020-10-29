3c4af927853494e459ade941ea8043df99ca9950
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 16:35:32 +0900

    6.2.3 イベントをトリガーにして処理を実行

--- sample.js
+++ sample.js
@@ -1,20 +1,5 @@
-var s = document.getElementById('food');
-var opts = s.childNodes;
-for (var i = 0, len = opts.length; i < len; i++) {
-    var opt = opts.item(i);
-    if (opt.nodeType === 1) {
-        console.log(opt.value);
-    }
-}
-var child = s.firstChild;
-while (child) {
-    if (child.nodeType === 1) {
-        console.log(child.value);
-    }
-    child = child.nextSibling;
-}
-var child = s.firstElementChild;
-while (child) {
-    console.log(child.value);
-    child = child.nextElementSibling;
-}
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn').addEventListener('click', function () {
+        window.alert('ボタンがクリックされました');
+    }, false);
+}, false);
