150db2b14c07e97fc67446f8198745d44eb44b22
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 11:11:16 +0900

    プロパティの動的生成

--- sample.js
+++ sample.js
@@ -1,8 +1,10 @@
+let i = 0;
 let member = {
     name: '山田太郎',
     birth: new Date(1970, 5, 25),
-    toString() {
-        return this.name + ' 誕生日:' + this.birth.toLocaleDateString()
-    }
+    ['memo' + ++i]: '正規会員',
+    ['memo' + ++i]: '支部会長',
+    ['memo' + ++i]: '関東',
 };
-console.log(member.toString());
+console.log(member);
+// {name: "山田太郎", birth: Thu Jun 25 1970 00:00:00 GMT+0900 (日本標準時), memo1: "正規会員", memo2: "支部会長", memo3: "関東"}
