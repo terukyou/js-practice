113d8c27a6ca553b769aeeabaa150379e36874ef
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 17:27:18 +0900

    6.4.5 リストボックスの値取得

--- sample.js
+++ sample.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var setCheckValue = function (name, value) {
-        var elems = document.getElementsByName(name);
+    var getSelectValue = function (name) {
+        var result = [];
+        var elems = document.getElementById(name).options;
         for (var i = 0, len = elems.length; i < len; i++) {
             var elem = elems.item(i);
-            if (value.indexOf(elem.value) > -1) {
-                elem.checked = true;
+            if (elem.selected) {
+                result.push(elem.value);
             }
         }
-    };
-    setCheckValue('food', ['餃子', '焼肉']);
+        return result;
+    }
+    document.getElementById('btn').addEventListener('click', function () {
+        window.alert(getSelectValue('food'));
+    }, false);
 }, false);
 
