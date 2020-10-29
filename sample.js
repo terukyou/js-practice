db826fe3114655aaa1b7ee8cf8094f280ee98f53
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 10:45:46 +0900

    プロパティの定義

--- sample.js
+++ sample.js
@@ -4,6 +4,19 @@ class Member {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+    // プロパティの定義
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        this._firstName = value;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
     // メソッドの定義
     getName() {
         return this.lastName + this.firstName;
