20a1968e447068725a9cbab5c1e82108b6454e2e
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 10:50:06 +0900

    静的メソッドの定義(static)

--- sample.js
+++ sample.js
@@ -1,27 +1,9 @@
-class Member {
-    // コンストラクタの定義
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    // プロパティの定義
-    get firstName() {
-        return this._firstName;
-    }
-    set firstName(value) {
-        this._firstName = value;
-    }
-    get lastName() {
-        return this._lastName;
-    }
-    set lastName(value) {
-        this._lastName = value;
-    }
-    // メソッドの定義
-    getName() {
-        return this.lastName + this.firstName;
+// statusで静的メソッドを定義できる
+class Area{
+    static getTriangle(base, height) {
+        return base * height / 2;
     }
 }
-
-let m = new Member('太郎', '山田');
-console.log(m.getName());
+a = new Area();
+console.log(Area.getTriangle(10,5)); //25
+console.log(a.getTriangle(10,5)); //エラー
