c1787f05fcb67e4e144f9cafbcb54508f01bbf2b
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 10:28:15 +0900

    5.4.3 名前空間の作成

--- sample.js
+++ sample.js
@@ -1,49 +1,14 @@
-// プライベートメンバーはコンストラクター関数で定義 varで宣言(thisを使わない)
-// 特権メソッドを定義プライベートメンバーにアクセス
-function Triangle() {
-    // プライベートプロパティの定義
-    var _base;
-    var _height;
-    // アクセサリーメソッドをまとめる
-    Object.defineProperties(
-        this, {
-        base: {
-            // ゲッターメソッド(参照用のメソッド)
-            get: function () {
-                return _base;
-            },
-            // セッターメソッド(設定用のメソッド)
-            set: function (base) {
-                if (typeof tmp === 'number' && base > 0) {
-                    _base = base;
-                }
-            }
-        },
-        height: {
-            get: function () {
-                return _height;
-            },
-            set: function (height) {
-                if (typeof tmp === 'number' && height > 0) {
-                    _height = height;
-                }
-            }
-        }
+// 名前空間を定義(Wingsが未定義の時のみ)
+var Wings = Wings || {};
+Wings.Member = function (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+};
+Wings.Member.prototype = {
+    getName: function () {
+        return this.lastName + '' + this.firstName;
     }
-    );
-}
+};
 
-Triangle.prototype.getArea = function () {
-    return this.getBase() * this.getHeight() / 2;
-}
-
-var t = new Triangle();
-t._base = 10;
-t._height = 2;
-console.log(t.getArea()); //NaN
-
-t.setBase(10);
-t.setHeight(2);
-console.log(t.getBase()); //10
-console.log(t.getHeight()); //2
-console.log(t.getArea()); //10
+var mem = new Wings.Member('ひろし', '山田');
+console.log(mem.getName()); //山田ひろし
