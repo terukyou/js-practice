a0343853153340e2bb0b37ed182c212e1b777b6e
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 10:57:04 +0900

    クラスの継承、オーバーライド、superキーワード

--- sample.js
+++ sample.js
@@ -1,9 +1,40 @@
-// statusで静的メソッドを定義できる
-class Area{
-    static getTriangle(base, height) {
-        return base * height / 2;
+class Member {
+    // コンストラクタの定義
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    // プロパティの定義
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        this._firstName = value;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+    // メソッドの定義
+    getName() {
+        return this.lastName + this.firstName;
     }
 }
-a = new Area();
-console.log(Area.getTriangle(10,5)); //25
-console.log(a.getTriangle(10,5)); //エラー
+// クラスの継承
+class BusinessMember extends Member{
+    constructor(firstName, lastName, clazz) {
+        super(firstName, lastName); //基底クラスにプロパティの設定をゆだねる
+        this.clazz = clazz;
+    }
+    getName() { //MemberのgetNameを上書き
+        return super.getName() + '　役職:' + this.clazz;
+    }
+    work() {
+        return this.getName() + 'は働いています';
+    }
+}
+let bm = new BusinessMember('太郎', '山田','課長');
+console.log(bm.getName());
+console.log(bm.work());
