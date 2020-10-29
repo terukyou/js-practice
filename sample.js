018c6b93ea872feaf96ae85affa8f192d48bf185
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-29 09:35:59 +0900

    5.3.3 オブジェクトの型判定

--- sample.js
+++ sample.js
@@ -1,23 +1,28 @@
-// インスタンスが生成された時点でプロトタイプチェーンは固定される
 var Animal = function () { };
-Animal.prototype = {
-    walk: function () {
-        console.log('トコトコ');
-    }
-};
-var SuperAnimal = function () { };
-SuperAnimal.prototype = {
-    walk: function () {
-        console.log('だだだだだ');
-    }
-};
-var Dog = function () { };
 
-Dog.prototype = new Animal();
-var d1 = new Dog();
-d1.walk(); //トコトコ
+var Hamster = function () { };
+Hamster.prototype = new Animal();
 
-Dog.prototype = new SuperAnimal();
-var d2 = new Dog();
-d2.walk(); //だだだだ
-d1.walk(); //トコトコ
+var a = new Animal();
+var h = new Hamster();
+
+// constructor,instanceofは元の関数オブジェクトを取得
+console.log(a.constructor === Animal); //true
+console.log(h.constructor === Animal); //true
+console.log(h.constructor === Hamster); //false(プロトタイプ継承)
+
+// コンストラクタもプロトタイプも判定
+console.log(h instanceof Animal); //true
+console.log(h instanceof Hamster); //true
+
+// isPrototypeOfは参照元のプロトタイプを確認
+console.log(Hamster.prototype.isPrototypeOf(h)); //true
+console.log(Animal.prototype.isPrototypeOf(h)); //true
+
+// メンバーの有無を判定(in)
+var obj = {
+    hoge: function () { },
+    foo: function () { },
+};
+console.log('hoge' in obj); //true
+console.log('piyo' in obj); //false
