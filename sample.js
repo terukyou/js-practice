7429063e60b8e55596a3edd43dfc0e512c2bfdfd
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-30 14:04:29 +0900

    6.6.3 classListプロパティ toggleメソッド

--- sample.js
+++ sample.js
@@ -1,28 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var list = document.getElementById('list');
-    var pic = document.getElementById('pic');
-    var del = document.getElementById('del');
+    var elem = document.getElementById('elem');
 
-    list.addEventListener('click', function (e) {
-        var isbn = e.target.getAttribute('data-isbn');
-        if (isbn) {
-            var img = document.createElement('img');
-            img.src = 'http://www.wings.msn.to/books/' + isbn + '/' + isbn + '.jpg';
-            img.alt = e.target.textContent;
-            img.height = 150;
-            img.width = 108;
-
-            if (pic.getElementsByTagName('img').length > 0) {
-                pic.replaceChild(img, pic.lastChild);
-            } else {
-                del.disabled = false;
-                pic.appendChild(img);
-            }
-        }
-    }, false);
-    del.addEventListener('click', function () {
-        pic.removeChild(pic.lastChild);
-        del.disabled = true;
+    elem.addEventListener('click', function () {
+        this.classList.toggle('highlight');
     }, false);
 }, false);
 
