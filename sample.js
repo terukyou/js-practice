4ad4bb644e07c4c753239a7a3515d4a267abe456
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-30 14:36:49 +0900

    6.7.4 イベントリスナー配下のthis

--- sample.js
+++ sample.js
@@ -1,18 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //false→パブリングフェーズ(発生元の下位要素から上位要素へイベントを伝播)#inner#inner2#outer
-    //true→キャプチャフェーズ(windowオブジェから下位要素にイベントを伝播)#outer#inner#inner2
-    document.getElementById('inner').addEventListener('click', function (e) {
-        window.alert('#inner リスナーが発生しました');
-        // e.stopPropagation(); //#inner #inner2が発生
-        // e.stopImmediatePropagation(); //#innerだけ発生
-        // e.preventDefault();//#inner#inner2#outerが発生するが、リンクは飛ばない
-    }, false);
-    document.getElementById('inner').addEventListener('click', function (e) {
-        window.alert('#inner リスナー2が発生しました');
-    }, false);
-    document.getElementById('outer').addEventListener('click', function (e) {
-        window.alert('#outer リスナーが発生しました');
-    }, false);
+    var data = {
+        title: 'Javaポケットリファレンス',
+        price: 2680,
+        handleEvent: function () { //handleEventメソッドを作成することで↓
+            console.log(this.title + '/' + this.price + '円');
+        }
+    };
+    //第二引数にオブジェクトを指定できる(これがないとthisがボタン元からになる)
+    document.getElementById('btn').addEventListener('click', data, false);
 }, false);
 
 
