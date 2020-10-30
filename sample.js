3a402c2623e0e47772cd785e09fcbc3d4f215589
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-10-30 14:27:15 +0900

    6.7.3 イベント処理の伝播とキャンセル

--- sample.js
+++ sample.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elem = document.getElementById('elem');
-
-    elem.addEventListener('click', function () {
-        this.classList.toggle('highlight');
+    //false→パブリングフェーズ(発生元の下位要素から上位要素へイベントを伝播)#inner#inner2#outer
+    //true→キャプチャフェーズ(windowオブジェから下位要素にイベントを伝播)#outer#inner#inner2
+    document.getElementById('inner').addEventListener('click', function (e) {
+        window.alert('#inner リスナーが発生しました');
+        // e.stopPropagation(); //#inner #inner2が発生
+        // e.stopImmediatePropagation(); //#innerだけ発生
+        // e.preventDefault();//#inner#inner2#outerが発生するが、リンクは飛ばない
+    }, false);
+    document.getElementById('inner').addEventListener('click', function (e) {
+        window.alert('#inner リスナー2が発生しました');
+    }, false);
+    document.getElementById('outer').addEventListener('click', function (e) {
+        window.alert('#outer リスナーが発生しました');
     }, false);
 }, false);
 
